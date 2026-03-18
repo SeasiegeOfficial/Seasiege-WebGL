c2d18fb8d740d54933769ac6ba5e1393683ae986
Lessyzz <leviaworksstudio@gmail.com>
2026-03-19 01:26:44 +0300

    Update

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,4 +1,4 @@
-const cacheName = "Leviaworks-Seasiege-1.0.4.1";
+const cacheName = "Leviaworks-Seasiege-1.0.4.2";
 const contentToCache = [
     "Build/Web.loader.js",
     "Build/Web.framework.js",
